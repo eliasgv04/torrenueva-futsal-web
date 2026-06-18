f84e3f1527708a31d84dee69db7bf2d3a1f8c686
Elías González Valdepeñas <elias.gonzalez@alu.uclm.es>
2026-06-18 10:40:59 +0200

    RRSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://torrenuevafutsal.es', // Cambia esta URL cuando tengas dominio propio
   integrations: [sitemap()],
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }),
 });
